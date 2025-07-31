0a1b747594f8afa788c282cd06a326a60359d48e
Максим Сюй <60548953+MaximSyuj@users.noreply.github.com>
2025-08-01 04:30:57 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿#include "Chat.h"
+
+int main()
+{
+	Chat chat;
+
+	chat.run();
+
+	while (chat.chatRun())
+	{
+		chat.mainMenu();
+
+		while (chat.getCurrentUser())
+		{
+			chat.userMenu();
+		}
+	}
+
+	return 0;
+}
